28cacbf5c921751ae9cd901642fa7e7993c4be58
Ramic <ramlag1819@gmail.com>
2023-07-07 23:31:33 -0700

    Quick sort implementation

--- algorithms/sorting/intermediate-algos/quick-sort.js
+++ algorithms/sorting/intermediate-algos/quick-sort.js
@@ -56,7 +56,7 @@
 // - Swap the starting element (i.e. the pivot) with the pivot index
 // - Return the pivot index
 
-function pivot(arr, start = 0, end = arr.length - 1) {
+function pivot(arr, start = 0, end = arr.length) {
   let pivot = arr[0];
   let pivotIndex = 0;
 
@@ -73,3 +73,38 @@ function pivot(arr, start = 0, end = arr.length - 1) {
 }
 
 pivot([3,1,4,2,5]);
+
+// Colt's solution
+function pivot(arr, start = 0, end = arr.length - 1) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+  };
+
+  let pivot = arr[start];
+  let swapIdx = start;
+
+  for (let i = start + 1; i <= end; i++) {
+    if (pivot > arr[i]) {
+      swapIdx++;
+      swap(arr, swapIdx, i)
+    }
+  }
+  swap(arr, start, swapIdx);
+  return swapIdx;
+}
+
+// Quicksort Pseudocode
+// - Call the pivot helper on the array
+// - When the helper returns to you the updated pivot index, recursively call the pivot helper on
+// the subarray to the left of that index, and the subarray to the right of that index
+// - Your base case occurs when you consider a subarray with less than 2 elements
+
+function quickSort(arr, left = 0, right = arr.length - 1) {
+  if (left < right) {
+    let pivotIndex = pivot(arr, left, right);
+    quickSort(arr,left, pivotIndex - 1);
+    quickSort(arr,pivotIndex + 1, right);
+  }
+
+  return arr;
+}
